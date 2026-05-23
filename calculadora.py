1e3a5e1a4f9be01cefad3fa436d38601f325511b
LiAgustin <113304673+LiAgustin@users.noreply.github.com>
2026-05-22 23:45:31 -0600

    fix: corregir espaciado entre funciones E302

--- calculadora.py
+++ calculadora.py
@@ -1,22 +1,23 @@
 # calculadora.py — Lizbeth San Agustin Vergara
 
-
 def sumar(a, b):
     """Devuelve la suma de dos números."""
     return a + b
+
 
 def restar(a, b):
     """Devuelve la resta de dos números."""
     return a - b
 
+
 def multiplicar(a, b):
     """Devuelve la multiplicación."""
     return a * b
+
 
 def dividir(a, b):
     """Devuelve la división."""
     if b == 0:
         raise ValueError("No se puede dividir entre cero")
     return a / b
-
 
